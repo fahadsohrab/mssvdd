fe1595d58794aaa8ba064c6a10883104feb4468e
Fahad Sohrab <36382470+fahadsohrab@users.noreply.github.com>
2020-09-10 19:35:59 +0300

    Update decissionmssvdd.m

--- decissionmssvdd.m
+++ decissionmssvdd.m
@@ -1,7 +1,14 @@
+% This fucntion is for implemention the decision strategies for "Multimodal subspace support vector data description"
+% Input:  predict_label1 = Predicted labels from modality 1
+%         predict_label2 = Predicted labels from modality 2
+% Output: Decission1 = Decision strategy 1 (also called the AND gate)
+%         Decission2 = Decision strategy 2 (also called as the OR gate)
+%         Decission3 = The final classifcation decision is made on the basis of first modality
+%         Decission4 = The final classifcation decision is made on the basis of second modality
 
 function [Decission1,Decission2,Decission3,Decission4] = decissionmssvdd(predict_label1,predict_label2)
 
-%Decission making (AND GATE)i call it decission 1
+%Decission making (AND GATE)
 Decission1=(predict_label1+predict_label2);
 Decission1(Decission1==0) = -1;
 Decission1(Decission1==2) = 1;
