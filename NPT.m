3195b008c93738862e5e08d3b55ff4fa01f2976e
Fahad Sohrab <36382470+fahadsohrab@users.noreply.github.com>
2020-09-10 19:33:17 +0300

    Update NPT.m

--- NPT.m
+++ NPT.m
@@ -1,4 +1,11 @@
- function [Phi,Phi_t] = NPT(M_train,M_test,kappa)
+% This fucntion implements NON-Linear Projection Trick (NPT)
+% Input:  M_train = Train data
+%         M_test = Test data
+%         kappa = Hyperparameter which determines the width of the kernel
+% Output: Phi = Output Phi train
+%         Phi_t = Output Phi test
+
+function [Phi,Phi_t] = NPT(M_train,M_test,kappa)
 
 %%RBF kernel
 % % function [Ktrain, Ktest] = kernel_rbf(M_train, M_test, kappa)
@@ -10,16 +17,15 @@ sigma2 = kappa * mean(mean(Dtrain));  A = 2.0 * sigma2;
 Ktrain = exp(-Dtrain/A);  		      Ktest = exp(-Dtest/A);
 
 % % center_kernel_matrices
-% % % function [Ktrain,Ktest] = center_kernel_matrices(Ktrain,Ktest)
 N = size(Ktrain,2);  M = size(Ktest,2);
 Ktest = (eye(N,N)-ones(N,N)/N) * (Ktest - (Ktrain*ones(N,1)/N)*ones(1,M));
 Ktrain = (eye(N,N)-ones(N,N)/N) * Ktrain * (eye(N,N)-ones(N,N)/N);
-            
 
-% % % % % %  function [Phi,Phi_t] = NPT(Ktrain,Ktest,dd)
-[U,S] = eig(Ktrain);        s = diag(S);                s(s<0) = 0.0;  
+
+% % Get Phi,Phi_t
+[U,S] = eig(Ktrain);        s = diag(S);                s(s<0) = 0.0;
 [U, s] = sortEigVecs(U,s);  s_acc = cumsum(s)/sum(s);   S = diag(s);
 
 Pmat = pinv(( S^(0.5)*U')');  if isreal(Pmat)==0,  Pmat = abs(Pmat);  end
-Phi = Pmat*Ktrain;                      Phi_t = pinv(Phi')*Ktest;   
- end
+Phi = Pmat*Ktrain;                      Phi_t = pinv(Phi')*Ktest;
+end
