2cb7bcfc4da6fc982429fdc16d5e47f9531a179e
Fahad Sohrab <36382470+fahadsohrab@users.noreply.github.com>
2020-09-10 19:36:40 +0300

    Update kernel_rbf.m

--- kernel_rbf.m
+++ kernel_rbf.m
@@ -1,3 +1,10 @@
+% This fucntion is used for getting K matrices to implement kernel trick.
+% Input:  M_train = Train data
+%         M_test = Test data
+%         kappa = Hyperparameter which determines the width of the kernel
+% Output: Ktrain = Output K train matrix
+%         Ktest = Output K test matrix
+
 function [Ktrain, Ktest] = kernel_rbf(M_train, M_test, kappa)
 
 N = size(M_train,2);  NN = size(M_test,2);
@@ -6,3 +13,4 @@ Dtest = ((sum(M_train'.^2,2)*ones(1,NN))+(sum(M_test'.^2,2)*ones(1,N))'-(2*(M_tr
 
 sigma2 = kappa * mean(mean(Dtrain));  A = 2.0 * sigma2;
 Ktrain = exp(-Dtrain/A);  		      Ktest = exp(-Dtest/A);
+end
