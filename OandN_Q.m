ea84929dacdbe244c095ec60d60689094d8c747a
Fahad Sohrab <36382470+fahadsohrab@users.noreply.github.com>
2020-09-10 19:34:00 +0300

    Update OandN_Q.m

--- OandN_Q.m
+++ OandN_Q.m
@@ -1,7 +1,10 @@
-% Orthogonalize and normalize the matrix
+% This fucntion Orthogonalize and normalize the projection matrix Q
+% Input:  Q = The projection matrix
+% Output: Q = Orthogonalize and normalize the projection matrix Q
+
 function Q = OandN_Q(Q)
-% % % Orthogonalize Q
-[tmpQ, R]=qr(Q',0);  Q = tmpQ';
+%%% Orthogonalize Q
+[tmpQ, ~]=qr(Q',0);  Q = tmpQ';
 clear tmpQ;          clear R;
 
 %%% Normalize each projection vector
