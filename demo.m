6adea8d1ef7c55e299190b6c793eb693303aac26
Fahad Sohrab <36382470+fahadsohrab@users.noreply.github.com>
2020-09-10 19:47:43 +0300

    Update demo.m

--- demo.m
+++ demo.m
@@ -1,27 +1,24 @@
+% This is a sample demo code for Multimodal Subspace Support Vector Data Description
+% The demo code is provided for 2 modalities Linear case MS-SVDD
+% Please contact fahad.sohrab@tuni.fi for any errors/bugs
 clc
 close all
 clear
 
-% This is a sample demo code for Multimodal Subspace Support Vector Data Description
-% The demo code is provided for 2 modalities Linear case MS-SVDD
-% Please contact fahad.sohrab@tuni.fi for any errors/bugs 
-
 %%Generate Random Data
 noOfTrainData = 500; noOfTestData = 100;
-D{1} = 5; D{2}= 4; %Original dimentionality differnet modalities 
-         Traindata{1} = rand(D{1},noOfTrainData); %Training Data/Features from modality 1
-         Traindata{2} = rand(D{2},noOfTrainData); %Training Data/Features from modality 2
-    trainlabels = ones(noOfTrainData,1); %Training labels (all +1s)
-        
-        testlabels = -ones(noOfTestData,1);
-        perm = randperm(noOfTestData);
-        positiveSamples = floor(noOfTestData/2);
-    testlabels(perm(1:positiveSamples))=1; % test labels, +1 for target, -1 for outliers
-        Testdata{1} = rand(D{1},noOfTestData); %Testing Data/Features from modality 1
-        Testdata{2} = rand(D{2},noOfTestData); %Testing Data/Features from modality 2
+D{1} = 5; D{2}= 4; %Original dimentionality differnet modalities
+Traindata{1} = rand(D{1},noOfTrainData); %Training Data/Features from modality 1
+Traindata{2} = rand(D{2},noOfTrainData); %Training Data/Features from modality 2
+trainlabels = ones(noOfTrainData,1); %Training labels (all +1s)
 
- 
- 
+testlabels = -ones(noOfTestData,1);
+perm = randperm(noOfTestData);
+positiveSamples = floor(noOfTestData/2);
+testlabels(perm(1:positiveSamples))=1; % test labels, +1 for target, -1 for outliers
+Testdata{1} = rand(D{1},noOfTestData); %Testing Data/Features from modality 1
+Testdata{2} = rand(D{2},noOfTestData); %Testing Data/Features from modality 2
+
 %%Setting hyperparameters, regularization term(omega) and decission type
 omega=1; % 0 for no regularizaterm used other options 1,2,3,4,5,6
 DecissionNumber=1; %1=AND, 2=OR, 3=1st modality, 4=2nd modality
@@ -40,7 +37,7 @@ sigma=10^-3;    %For non-linear usage
 
 RedTestdata1=Q{1} * Testdata{1}; %Test Data1 after maping from D to d
 RedTestdata2=Q{2} * Testdata{2}; %Test Data2 after maping from D to d
-    
+
 predict_label1 = svmpredict(testlabels, RedTestdata1', Model);
 predict_label2 = svmpredict(testlabels, RedTestdata2', Model);
 
